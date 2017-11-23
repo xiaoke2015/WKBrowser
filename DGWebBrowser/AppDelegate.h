c30786f0c8d6f1a7def0165c44564ad413f1b0f2
李加建 <779454060@qq.com>
2017-11-23 09:56:29 +0800

    Initial Commit

---
+++ DGWebBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DGWebBrowser
+//
+//  Created by 李加建 on 2017/11/23.
+//  Copyright © 2017年 jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
